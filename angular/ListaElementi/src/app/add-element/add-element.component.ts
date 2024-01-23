dfc43e7f8c56e66178824254bbdd2a384fcd9165
GOCA <goca@moon.corner.tech>
2024-01-23 08:13:23 +0100

    created Angular folder and a new Angular project

---
+++ angular/ListaElementi/src/app/add-element/add-element.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-element',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-element.component.html',
+  styleUrl: './add-element.component.css'
+})
+export class AddElementComponent {
+
+}
---
+++ angular/ListaElementi/src/app/element-list/element-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-element-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './element-list.component.html',
+  styleUrl: './element-list.component.css'
+})
+export class ElementListComponent {
+
+}
